81da0667221badb4c6e7e369a68feb149d571bf3
voncardoso <vonharrison26@gmail.com>
2022-03-18 13:18:20 -0300

    listagem mobile finalizada ate 600px

--- src/components/Transactions/index.js
+++ src/components/Transactions/index.js
@@ -2,39 +2,41 @@ import './style.css';
 
 const Transactions = () =>{
     return(
-        <section className='Transactionstable'>
-            <table>
-                <thead>
-                     <tr>
-                         <th>Titulo</th>
-                         <th>Valor</th>
-                         <th>Categoria</th>
-                         <th>Data</th>
-                     </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Lanche</td>
-                        <td>R$ 15.000,00</td>
-                        <td>Saida</td>
-                        <td>18/03/2022</td>
-                    </tr>
-                    <tr>
-                        <td>Lanche</td>
-                        <td>R$ 15.000,00</td>
-                        <td>Saida</td>
-                        <td>18/03/2022</td>
-                    </tr>
-                    <tr>
-                        <td>Lanche</td>
-                        <td>R$ 15.000,00</td>
-                        <td>Saida</td>
-                        <td>18/03/2022</td>
-                    </tr>
-                </tbody>
-            </table>
-        </section>
+        <>
+            <section className='Transactionstable'>
+                <table className='tableList'>
+                    <thead>
+                        <tr>
+                            <th>Titulo</th>
+                            <th>Valor</th>
+                            <th>Categoria</th>
+                            <th>Data</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Lanche</td>
+                            <td>R$ 15.000,00</td>
+                            <td>Saida</td>
+                            <td>18/03/2022</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </section>
 
+            <section className='TransactionstableMobile'>
+                <h3>Listagem</h3>
+
+                <div>
+                    <p>Desenvolvimento de site</p>
+                    <strong className='entradaMobile'>R$ 15.00,00</strong>
+                    <ul>
+                        <li>Vendas</li>
+                        <li>12/05/2022</li>
+                    </ul>
+                </div>
+            </section>
+        </>
     )
 }
 
